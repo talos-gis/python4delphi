fe303668fab783b239cf1151729156a4da69b40e
Idan Miara <idan@miara.com>
2025-05-07 11:33:44 +0300

    PythonEngine.pas - add GetPythonPathAsStrings

--- Source/PythonEngine.pas
+++ Source/PythonEngine.pas
@@ -2033,6 +2033,8 @@ type
 
   TPythonType = class; //forward declaration
 
+  TWhichPythonPaths = (wppAll, wppOnlyCustom, wppOnlyBase);
+
   {$IF not Defined(FPC) and (CompilerVersion >= 23)}
   [ComponentPlatformsAttribute(pidSupportedPlatforms)]
   {$IFEND}
@@ -2069,6 +2071,7 @@ type
     FPyDateTime_DateTimeTZType:  PPyObject;
 
   protected
+    FBasePythonPath: TStrings;
     procedure  Initialize;
     procedure  Finalize;
     procedure AfterLoad; override;
@@ -2091,6 +2094,7 @@ type
     // Constructors & Destructors
     constructor Create(AOwner: TComponent); override;
     destructor  Destroy; override;
+    procedure GetPythonPathAsStrings(Strings: TStrings; WhichPaths: TWhichPythonPaths=wppAll);
 
     // Public methods
     procedure  SetPythonHome(const PythonHome: UnicodeString);
@@ -4640,6 +4644,7 @@ begin
   FUseWindowsConsole       := False;
   FPyFlags                 := DEFAULT_FLAGS;
   FDatetimeConversionMode  := DEFAULT_DATETIME_CONVERSION_MODE;
+  FBasePythonPath          := TStringList.Create;
   if csDesigning in ComponentState then
     begin
       for i := 0 to AOwner.ComponentCount - 1 do
@@ -4662,6 +4667,7 @@ begin
   FClients.Free;
   FInitScript.Free;
   FTraceback.Free;
+  FBasePythonPath.Free;
   inherited;
 end;
 
@@ -4714,10 +4720,36 @@ begin
   FPyDateTime_DateTimeTZType  := nil;
 end;
 
+procedure TPythonEngine.GetPythonPathAsStrings(Strings: TStrings; WhichPaths: TWhichPythonPaths);
+var
+  list, obj2: PPyObject;
+  i: integer;
+  item: String;
+  ReturnAll: boolean;
+begin
+  case WhichPaths of
+    wppAll, wppOnlyCustom: begin
+      ReturnAll := WhichPaths = wppAll;
+      Strings.Clear;
+      list := self.PySys_GetObject('path');
+      for i := 0 to PyList_Size(list)-1 do begin
+        obj2 := PyList_GetItem(list, i);
+        item := PyObjectAsString(obj2);
+        if ReturnAll or not FBasePythonPath.Contains(item) then
+          Strings.Add(item);
+      end;
+    end;
+    wppOnlyBase: begin
+      Strings.SetStrings(FBasePythonPath)
+    end;
+  end;
+end;
+
 procedure TPythonEngine.AfterLoad;
 begin
   inherited;
   Initialize;
+  GetPythonPathAsStrings(FBasePythonPath, wppAll);
 end;
 
 procedure TPythonEngine.BeforeLoad;
